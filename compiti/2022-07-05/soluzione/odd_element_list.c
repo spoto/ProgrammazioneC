e6913995a66c4d705915bf27ac4e978b7881b5db
Fausto Spoto <fausto.spoto@gmail.com>
2022-07-07 18:15:42 +0200

    Semplificata la soluzione del compito del 5 luglio 2022

--- compiti/2022-07-05/soluzione/odd_element_list.c
+++ compiti/2022-07-05/soluzione/odd_element_list.c
@@ -2,72 +2,60 @@
 #include <stdlib.h>
 
 struct element_t {
-    int value;
-    struct element_t *next;
+  int value;
+  struct element_t *next;
 };
 
 struct element_t *create_odd_list(const char *filename);
 void print_list(struct element_t *list);
 
 int main() {
-    char filename[] = "numbers.txt";
-    struct element_t *list = create_odd_list(filename);
-    print_list(list);
-    return 0;
+  char filename[] = "numbers.txt";
+  struct element_t *list = create_odd_list(filename);
+  print_list(list);
+  return 0;
 }
 
 struct element_t *create_odd_list(const char *filename) {
-    struct element_t *current = NULL;
-    struct element_t *prev = NULL;
-    struct element_t *new = NULL;
-    struct element_t *head = NULL;
+  struct element_t *first = NULL;
+  struct element_t *last = NULL;
 
-    FILE *fp = fopen(filename, "r");
+  FILE *fp = fopen(filename, "r");
 
-    if (fp == NULL) {
-        printf("Errore nell'apertura del file: %s\n", filename);
-        exit(1);
+  if (fp == NULL) {
+    printf("Errore nell'apertura del file: %s\n", filename);
+    exit(1);
+  }
+  
+  int value;
+  fscanf(fp, "%d", &value);
+
+  while (!feof(fp)) {
+    if (value % 2 != 0) {
+      struct element_t *new = (struct element_t *) malloc(sizeof(struct element_t));
+      
+      if (new) {
+	new -> value = value;
+	new -> next = NULL;
+
+	if (last == NULL)
+	  first = last = new;
+	else
+	  last = last -> next = new;
+      }
     }
 
-    int value;
     fscanf(fp, "%d", &value);
+  }
 
-    while (!feof(fp)) {
-        if (value % 2 != 0) {
-            new = (struct element_t *)malloc(sizeof(struct element_t));
+  fclose(fp);
 
-            if (new) {
-                if (head == NULL) {
-                    head = new;
-                    new->next = NULL;
-                } else {
-                    current = head;
-                    prev = head;
-
-                    while (current) {
-                        prev = current;
-                        current = current->next;
-                    }
-
-                    prev->next = new;
-                }
-
-                new->value = value;
-            }
-        }
-        fscanf(fp, "%d", &value);
-    }
-    fclose(fp);
-    return head;
+  return first;
 }
 
 void print_list(struct element_t *list) {
-    struct element_t *current = list;
+  for (struct element_t *current = list; current != NULL; current = current -> next)
+    printf("%i ", current->value);
 
-    while (current != NULL) {
-        printf("%i ", current->value);
-        current = current->next;
-    }
-
-    printf("\n");
+  printf("\n");
 }
