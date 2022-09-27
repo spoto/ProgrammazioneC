52f7179c2dceb5444f7c3cb8bc8ba9302ec2aab6
Fausto Spoto <fausto.spoto@gmail.com>
2022-09-27 09:48:06 +0200

    Corretto commento

--- compiti/2022-09-26/soluzione/bits.c
+++ compiti/2022-09-26/soluzione/bits.c
@@ -26,7 +26,7 @@ int swap(int array[], int length) {
   int ho_fatto_scambio = 0;
   for (int pos = 0; pos < length - 1; pos++)
     if (population(array[pos]) > population(array[pos + 1])) {
-      // scambia(array[pos] con array[pos + 1])
+      // scambia array[pos] con array[pos + 1]
       int temp = array[pos];
       array[pos] = array[pos + 1];
       array[pos + 1] = temp;
